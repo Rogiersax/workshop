1ded177395262f399880b0ca37dfff18b2417eea
rho13 <rogiersaxion@gmail.com>
2014-11-24 09:46:12 +0100

    update

--- BranchTest.java
+++ BranchTest.java
@@ -3,4 +3,5 @@ public static void main(String [] args) {
 	System.out.println("Lokal repo from PCB !!");
 	System.out.println("Update from PCA  !!");
 	System.out.println("Update to newFeatureBranch. From PCB  !!");
+	System.out.println("Update to newFeatureBranch. From PCB another");
 }
